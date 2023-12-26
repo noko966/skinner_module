d77c79cd77d7d67d4266a7d5baac6288f11d0c8d
nik <nikolay.kananov@digitain.com>
2023-12-26 19:04:02 +0400

    merging

--- Js2/Skinne2.js
+++ Js2/Skinne2.js
@@ -1,8 +1,14 @@
+
 class TreeNode {
   constructor(name, parent = null) {
     this.name = name;
     this.children = [];
     this.parent = parent;
+
+    this.skin = {
+
+    }
+
   }
 
   addChild(childNode) {
@@ -41,8 +47,9 @@ let brand = new TreeNode("brand");
 let button = new TreeNode("button");
 
 body.addChild(dominant); /* body */
-body.addChild(evt); /* body */
+dominant.addChild(evt); /* body */
 dominant.addChild(menu_1);
+evt.addChild(header);
 evt.addChild(buttonSecondary);
 header.addChild(subHeader);
 menu_1.addChild(menu_2);
@@ -64,11 +71,13 @@ class Skinner2 {
         bg2: 8,
         bg3: 8,
         bgHov: 3,
+        step: 4,
       },
       light: {
         bg2: 8,
         bg3: 8,
         bgHov: 3,
+        step: 4,
       },
       alpha: {
         bg: 0.6,
@@ -76,13 +85,177 @@ class Skinner2 {
         bg3: 0.2,
       },
       txt: {
-        txt: 0.9,
-        txt2: 0.6,
-        txt3: 0.4,
+        txt: 10,
+        txt2: 20,
+        txt3: 40,
       },
     };
+
     this.getConfigBlueprint = this.getConfigBlueprint.bind(this);
+    this.generateConfigFromInput = this.generateConfigFromInput.bind(this);
     this.mergeConfig = this.mergeConfig.bind(this);
+
+  }
+
+  addHTMLDoomy() {
+    let self = this;
+
+    let style = document.getElementById("skinnerHTMLStyle");
+    if (!style) {
+      style = document.createElement("style");
+      style.id = "skinnerHTMLStyle";
+      document.head.appendChild(style);
+    }
+
+    let styles = "";
+    styles += `
+    body{
+      margin: 0;
+    }
+    * {
+      box-sizing: border-box;
+    }
+    .skinner_HTML_container {
+      font-family: arial;
+      display: grid;
+      grid-template-columns: repeat(5, 1fr);
+      grid-gap: 32px;
+      padding: 32px;
+    }
+    .skinner_HTML_box_heading{
+      font-size: 20px;
+      text-transform: capitalize;
+      font-weight: 700;
+      text-align: left;
+      padding: 0 16px;
+    }
+    .skinner_HTML_box_container{
+      padding: 8px;
+      border-radius: 8px;
+    }
+    .skinner_HTML_box {
+      padding: 8px 16px;
+      display: grid;
+      grid-gap: 6px;
+      grid-template-rows: repeat(3, 1fr);
+      font-weight: 700;
+      font-size: 14px;
+    }
+    .skinner_HTML_box:first-child{
+      border-top-left-radius: 4px;
+      border-top-right-radius: 4px;
+    }
+    .skinner_HTML_box:last-child{
+      border-bottom-left-radius: 4px;
+      border-bottom-right-radius: 4px;
+    }
+    `;
+
+    // Create a style element
+    let container = document.createElement("div");
+    container.className = 'skinner_HTML_container';
+    document.body.appendChild(container);
+
+    function createEssenceHTML(bg, txt, txt2, txt3) {
+      let essenceBg = document.createElement("div");
+      essenceBg.className = `skinner_HTML_box`;
+      essenceBg.style.backgroundColor = bg;
+      let essenceTxt = document.createElement("div");
+      essenceTxt.innerText = 'important text content';
+      essenceTxt.style.color = txt;
+      let essenceTxt2 = document.createElement("div");
+      essenceTxt2.style.color = txt2;
+      essenceTxt2.innerText = 'text content';
+      let essenceTxt3 = document.createElement("div");
+      essenceTxt3.style.color = txt3;
+      essenceTxt3.innerText = 'minor text content';
+      essenceBg.appendChild(essenceTxt);
+      essenceBg.appendChild(essenceTxt2);
+      essenceBg.appendChild(essenceTxt3);
+      return essenceBg;
+    }
+
+    function addHTMLGroup(node) {
+      let vd = self.verbalData(node.name);
+
+      // Create a style element
+
+      let essenceContainer = document.createElement("div");
+      essenceContainer.className = 'skinner_HTML_box_container';
+      essenceContainer.style.background = `var(--${vd["nameBg"]})`;
+      container.appendChild(essenceContainer);
+
+
+      let essenceHeading = document.createElement("div");
+      essenceHeading.className = 'skinner_HTML_box_heading';
+      essenceHeading.style.color = `var(--${vd["nameTxt"]})`;
+      essenceHeading.innerText = `${vd["name"]}`;
+      essenceContainer.appendChild(essenceHeading);
+
+      let bg = createEssenceHTML(`var(--${vd["nameBg"]})`, `var(--${vd["nameTxt"]})`, `var(--${vd["nameTxt2"]})`, `var(--${vd["nameTxt3"]})`);
+      let bg1 = createEssenceHTML(`var(--${vd["nameBg2"]})`, `var(--${vd["nameTxt"]})`, `var(--${vd["nameTxt2"]})`, `var(--${vd["nameTxt3"]})`);
+      let bg2 = createEssenceHTML(`var(--${vd["nameBg3"]})`, `var(--${vd["nameTxt"]})`, `var(--${vd["nameTxt2"]})`, `var(--${vd["nameTxt3"]})`);
+
+
+      essenceContainer.appendChild(bg);
+      essenceContainer.appendChild(bg1);
+      essenceContainer.appendChild(bg2);
+
+      if (node.children && node.children.length > 0) {
+        node.children.forEach(child => {
+          addHTMLGroup(child); // Recursive call
+        });
+      }
+    }
+
+    this.rootNodes.forEach(node => {
+      addHTMLGroup(node);
+    })
+
+    style.textContent = styles;
+  }
+
+  addHTMLVars() {
+    let self = this;
+    // Create a style element
+    let style = document.getElementById("skinnerVars");
+    if (!style) {
+      style = document.createElement("style");
+      style.id = "skinnerVars";
+      document.head.appendChild(style);
+    }
+
+    let styles = "";
+    styles += `:root{\n`;
+    // Generate CSS custom properties
+
+
+    function addVariableGroup(node) {
+      let vd = self.verbalData(node.name);
+
+      styles += `--${vd["nameBg"]}: ${self.skin[vd["nameBg"]]};\n`;
+      styles += `--${vd["nameBg2"]}: ${self.skin[vd["nameBg2"]]};\n`;
+      styles += `--${vd["nameBg3"]}: ${self.skin[vd["nameBg3"]]};\n`;
+
+      styles += `--${vd["nameTxt"]}: ${self.skin[vd["nameTxt"]]};\n`;
+      styles += `--${vd["nameTxt2"]}: ${self.skin[vd["nameTxt2"]]};\n`;
+      styles += `--${vd["nameTxt3"]}: ${self.skin[vd["nameTxt3"]]};\n`;
+
+      if (node.children && node.children.length > 0) {
+        node.children.forEach(child => {
+          addVariableGroup(child); // Recursive call
+        });
+      }
+    }
+
+    this.rootNodes.forEach(node => {
+      addVariableGroup(node);
+    })
+    styles += `}\n`;
+    style.textContent = styles;
+
+    // Append the style element to the head of the document
+    document.head.appendChild(style);
   }
 
   getConfigBlueprint() {
@@ -112,13 +285,16 @@ class Skinner2 {
     };
   }
 
-  generateConfigFromInput(inp) {
+
+
+  generateConfigFromInput(node) {
     /* Background.color: */
-    let incoming = inp.name;
+    let incoming = node.name;
     let incomingConfig = this.CFG[incoming] || {};
     let confPeace = {
       Background: {
         isActive: false,
+        isDark: false,
         color: "#212121",
       },
       Gradient: {
@@ -141,89 +317,54 @@ class Skinner2 {
       borderRadius: 4,
     };
 
-    // let mergedConfPeace = mergeConfigurations(confPeace, incomingConfig);
 
     let mergedConfPeace = { ...confPeace, ...incomingConfig };
 
-    console.log(mergedConfPeace, incomingConfig, inp.name);
+    node.cfg = mergedConfPeace;
   }
 
+
+
+  generateConfigMissingValues() {
+    let self = this;
+    function configFromParent(node) {
+      if (!node.cfg.Background.isActive) {
+        let parentValue = node.parent.cfg.Background.color;
+        let parentIsDark = self.TC(parentValue).isDark();
+        node.cfg.Background.isDark = parentIsDark;
+        node.cfg.Background.color = parentIsDark ? self.TC(parentValue).lighten(10).toHexString() : self.TC(parentValue).darken(10).toHexString();
+      }
+
+      if (node.children && node.children.length > 0) {
+        node.children.forEach(child => {
+          configFromParent(child); // Recursive call
+        });
+      }
+    }
+
+    this.rootNodes.forEach(node => {
+      configFromParent(node);
+    })
+  }
+
+
+
   mergeConfig() {
+    let self = this;
     let mergedConfig = {};
-    let blueprint = this.getConfigBlueprint();
+    function processNode(node) {
+      // Process the current node
+      self.generateConfigFromInput(node);
+      // If the node has children, process each child
+      if (node.children && node.children.length > 0) {
+        node.children.forEach(child => {
+          processNode(child); // Recursive call
+        });
+      }
+    }
 
-    /* body: {
-      Background: {
-        color: "#e5e5e5",
-        fromLight: !tinycolor("#e5e5e5").isDark(),
-        isDark: !tinycolor("#e5e5e5").isDark(),
-      },
-      Text: {
-        color: "#000",
-      },
-    },
-    dominant: {
-      Background: {
-        isActive: true,
-        color: "#999999",
-        isDark: !tinycolor("#999999").isDark(),
-      },
-      Text: {
-        color: "#000",
-      },
-    },
-    accent: {
-      Background: {
-        color: "#bd5726",
-        isDark: !tinycolor("#bd5726").isDark(),
-      },
-      Text: {
-        color: "#ffffff",
-      },
-    },
-    brand: {
-      Background: {
-        color: "#5ea2c9",
-        isDark: !tinycolor("#5ea2c9").isDark(),
-      },
-      Text: {
-        color: "#ffffff",
-      },
-    },
-  }, */
-
-    // const traverseAndMerge = (node) => {
-    //   let nodeConfig = this.CFG[node.name] || {};
-
-    //   mergedConfig[node.name] = { ...blueprint };
-    //   for (let key in blueprint) {
-    //     if (nodeConfig[key]) {
-    //       mergedConfig[node.name][key] = {
-    //         ...blueprint[key],
-    //         ...nodeConfig[key],
-    //       };
-    //     }
-    //   }
-    //   if (["body", "accent", "brand"].includes(node.name)) {
-    //     mergedConfig[node.name].Background.isActive = true;
-    //   }
-    //   node.children.forEach((childNode) => traverseAndMerge(childNode));
-    // };
-    // this.rootNodes.forEach((rootNode) => traverseAndMerge(rootNode));
-    // return mergedConfig;
-    this.rootNodes.forEach((rootNode) => {
-      rootNode.children.forEach((node) => {
-        if (!node) {
-          return;
-        }
-
-        if (node.children && node.children.length > 0) {
-          node.children.forEach((child) => {
-            this.generateConfigFromInput(child);
-          });
-        }
-      });
-      this.generateConfigFromInput(rootNode);
+    this.rootNodes.forEach((node) => {
+      processNode(node);
     });
   }
 
@@ -270,40 +411,50 @@ class Skinner2 {
     return data;
   }
 
-  traverse(root, action) {
-    if (!node) {
-      return;
+
+  makeColorPalette() {
+    let self = this;
+    function processNode(node) {
+      // Process the current node
+      self.makeBackgrounds(node);
+      self.makeText(node);
+      // If the node has children, process each child
+      if (node.children && node.children.length > 0) {
+        node.children.forEach(child => {
+          processNode(child); // Recursive call
+        });
+      }
     }
 
-    if (node.children && node.children.length > 0) {
-      node.children.forEach((child) => {
-        this.makeColorPalette(child);
-      });
-    }
-  }
+    this.rootNodes.forEach((node) => {
+      processNode(node);
+    });
 
-  makeColorPalette(node) {
-    if (!node) {
-      return;
-    }
-
-    this.makeBackgrounds(node);
-
-    if (node.children && node.children.length > 0) {
-      node.children.forEach((child) => {
-        this.makeColorPalette(child);
-      });
-    }
   }
 
   makeBackgrounds(node) {
+    let tc = this.TC;
+    let cfg = node.cfg;
+    let startColor = cfg.Background.color;
+    let isDark = cfg.Background.color.isDark;
     let vd = this.verbalData(node.name);
-    let en = vd.name;
-    let mc = this.getConfig();
-    let isActive = mc[en].Background.isActive;
-    let Bgc = mc[en].Background.color;
 
-    console.log(Bgc, node.name);
+    this.skin[vd.nameBg] = tc(startColor).toHexString();
+    this.skin[vd.nameBg2] = isDark ? tc(this.skin[vd.nameBg]).lighten(this.defaults.light.bg2).toHexString() : tc(this.skin[vd.nameBg]).darken(this.defaults.light.bg2).toHexString();
+    this.skin[vd.nameBg3] = isDark ? tc(this.skin[vd.nameBg2]).lighten(this.defaults.light.bg2).toHexString() : tc(this.skin[vd.nameBg2]).darken(this.defaults.light.bg2).toHexString();
+  }
+
+
+  makeText(node) {
+    let tc = this.TC;
+    let cfg = node.cfg;
+    let vd = this.verbalData(node.name);
+    let textColor = tc.mostReadable(this.skin[vd.nameBg], ["#ffffff", "#000000"]).toHexString();
+
+    this.skin[vd.nameTxt] = tc.mix(textColor, this.skin[vd.nameBg], this.defaults.txt.txt).toHexString();
+    this.skin[vd.nameTxt2] = tc.mix(textColor, this.skin[vd.nameBg], this.defaults.txt.txt2).toHexString();
+    this.skin[vd.nameTxt3] = tc.mix(textColor, this.skin[vd.nameBg], this.defaults.txt.txt3).toHexString();
+
   }
 
   setConfig(config) {
@@ -318,8 +469,12 @@ class Skinner2 {
   init() {
     document.body.style.background = "#212121";
     document.body.style.color = "#999999";
+    this.mergeConfig();
+    this.generateConfigMissingValues();
+    this.makeColorPalette();
 
-    let mergedConfigurations = this.mergeConfig(); // Assuming CFG is your incoming configuration
+    this.addHTMLVars();
+    this.addHTMLDoomy();
     // this.setConfig(mergedConfigurations);
     // let body = this.rootNodes[0];
     // this.rootNodes.forEach((node) => {
